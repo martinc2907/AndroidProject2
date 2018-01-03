50be47046f38027b97d43ba28f2ca507f45479d8
Sanghou <tkdgh97531@kaist.ac.kr>
2018-01-04 04:55:03 +0900

    add tab3

---
+++ app/src/main/java/com/example/user/project2/tab3_element.java
@@ -0,0 +1,48 @@
+package com.example.user.project2;
+
+/**
+ * Created by user on 2018-01-04.
+ */
+
+public class tab3_element {
+
+    private String type;
+
+    private String name;
+
+    private String number;
+
+    private String cash_only;
+
+    public String gettype(){
+        return type;
+    }
+
+    public void settype(String type){
+        this.type = type;
+    }
+
+    public String getname(){
+        return name;
+    }
+
+    public void setname(String name){
+        this.name = name;
+    }
+
+    public String getnumber(){
+        return number;
+    }
+
+    public void setnumber(String number){
+        this.number = number;
+    }
+
+    public String getcash_only(){
+        return cash_only;
+    }
+
+    public void setcash_only(String cash_only){
+        this.cash_only = cash_only;
+    }
+}
